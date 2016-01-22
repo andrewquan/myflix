f26eda1c49c2de418cf44081361750df620ccf86
Andrew Quan <andrew.h.quan@gmail.com>
2016-01-21 17:13:46 -0800

    Finalize Mailgun configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,8 +17,8 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
-  config.action_mailer.delivery_method = :stmp
-  config.action_mailer.default_url_options = { host: 'mighty-refuge-62959.herokuapp.com'}
+
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
