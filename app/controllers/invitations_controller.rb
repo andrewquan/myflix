5f2b459bdfd28f7925df8123024aa405e605c01f
Andrew Quan <andrew.h.quan@gmail.com>
2016-01-23 17:08:28 -0800

    update AppMailer to include object ids instead of objects for Sidekiq

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -9,7 +9,7 @@ class InvitationsController < ApplicationController
     @invitation = Invitation.new(invitation_params.merge!(inviter_id: current_user.id))
     
     if @invitation.save
-      AppMailer.delay.send_invitation_email(@invitation)
+      AppMailer.delay.send_invitation_email(@invitation.id)
       flash[:success] = "Your invitation to #{@invitation.recipient_name} was sent!"
       redirect_to new_invitation_path
     else
--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,16 +1,16 @@
 class AppMailer < ActionMailer::Base
-  def send_welcome_email(user)
-    @user = user
-    mail to: user.email, from: "info@myflix.com", subject: "Welcome to MyFlix!"
+  def send_welcome_email(user_id)
+    @user = User.find(user_id)
+    mail to: @user.email, from: "info@myflix.com", subject: "Welcome to MyFlix!"
   end
 
-  def send_forgot_password(user)
-    @user = user
-    mail to: user.email, from: "info@myflix.com", subject: "Reset password"
+  def send_forgot_password(user_id)
+    @user = User.find(user_id)
+    mail to: @user.email, from: "info@myflix.com", subject: "Reset password"
   end
 
-  def send_invitation_email(invitation)
-    @invitation = invitation
-    mail to: invitation.recipient_email, from: "info@myflix.com", subject: "Join MyFlix today!"
+  def send_invitation_email(invitation_id)
+    @invitation = Invitation.find(invitation_id)
+    mail to: @invitation.recipient_email, from: "info@myflix.com", subject: "Join MyFlix today!"
   end
 end
