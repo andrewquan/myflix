97a82e1da78d793bcea875cd92d5f60ae9543824
Andrew Quan <andrew.h.quan@gmail.com>
2016-02-01 18:10:03 -0800

    Add video_url and Watch Now functionality

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,6 @@ CarrierWave.configure do |config|
   config.aws_credentials = {
     access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region:            ENV['AWS_REGION']
   }
   if Rails.env.production?
     config.aws_bucket = ENV['AWS_S3_BUCKET_PRODUCTION']
